3ef3394c53511062f3a69f0c212f338f088e5937
matteosaponati <matteosaponati@gmail.com>
2025-08-12 11:54:02 +0200

    update argument parser

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -10,9 +10,9 @@ def main():
     ap.add_argument("--mode", 
                     type = str, 
                     choices = ["single", "iter"], default = "single")
-    ap.add_argument("--limit", type = int, default = 10000)
+    ap.add_argument("--limit", type = int, default = 1)
     ap.add_argument("--dataset", type = str, default = "gsm8k")
-    ap.add_argument("--load_in_4bit", type = bool, default = False)
+    ap.add_argument("--load_in_8bit", type = bool, default = False)
     ap.add_argument("--max_new_tokens", type = int, default = 384)
     ap.add_argument("--temperature", type = float, default = 0.9)
     ap.add_argument("--top_p", type = float, default = 0.9)
@@ -20,16 +20,15 @@ def main():
     ap.add_argument("--n", type = int, default = 1)
     ap.add_argument("--stop_sequences", type = str, nargs = "*", default = None) 
     ap.add_argument("--repetition_penalty", type = float, default = 1.0)
+    ap.add_argument("--torch_dtype", type = str, default = "auto")
     args = ap.parse_args()
     args.out_path = f"../datasets/{args.dataset}"
 
     dataset = list(iter_gsm8k(split = "train"))
     teacher = DataGenerator(
             model_name = args.model,
-            device = args.device,
             torch_dtype = args.torch_dtype,
-            cache_dir = args.cache_dir,
-            load_in_4bit = args.load_in_4bit,
+            load_in_8bit = args.load_in_8bit,
             max_new_tokens = args.max_new_tokens,
             temperature = args.temperature,
             top_p = args.top_p,
@@ -38,7 +37,8 @@ def main():
             stop_sequences = args.stop_sequences,
             repetition_penalty = args.repetition_penalty)
     teacher.synthesize(dataset = dataset,
-                       out_path = args.out_path)
+                       out_path = args.out_path,
+                       limit = args.limit)
     
 if __name__ == "__main__":
     main()
