bb32885cbc98bd7bed05ad0e680f7a1b4bd47c7a
matteosaponati <matteosaponati@gmail.com>
2025-08-15 09:27:17 +0200

    add --mode argument for switching between trippy and boring datasets, --dataset_name to allow multiple dataset, minor fix

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,6 +1,6 @@
 import argparse
-from src.synthetic_data.adapters.gsm8k import iter_gsm8k
 from src.synthetic_data.DataGenerator import DataGenerator
+from src.synthetic_data.adapters import iter_gsm8k
 
 def main():
     ap = argparse.ArgumentParser()
@@ -9,11 +9,11 @@ def main():
                     default = "meta-llama/Llama-3.2-3B-Instruct")
     ap.add_argument("--mode", 
                     type = str, 
-                    choices = ["single", "iter"], default = "single")
+                    default = "trippy")
     ap.add_argument("--limit", type = int, default = 8000)
     ap.add_argument("--dataset", type = str, default = "gsm8k")
     ap.add_argument("--load_in_8bit", type = bool, default = False)
-    ap.add_argument("--batch_size", type=int, default = 24)
+    ap.add_argument("--batch_size", type=int, default = 18)
     ap.add_argument("--max_new_tokens", type = int, default = 256)
     ap.add_argument("--temperature", type = float, default = 0.9)
     ap.add_argument("--top_p", type = float, default = 0.9)
@@ -22,12 +22,14 @@ def main():
     ap.add_argument("--repetition_penalty", type = float, default = 1.0)
     ap.add_argument("--torch_dtype", type = str, default = "auto")
     args = ap.parse_args()
-    args.out_path = f"../datasets/{args.dataset}-test"
+    args.out_path = f"../datasets/{args.dataset}-{args.mode}"
 
     dataset = list(iter_gsm8k(split = "train"))
     
+    ## create data generator
     teacher = DataGenerator(
             model_name = args.model,
+            dataset_name = args.dataset,
             torch_dtype = args.torch_dtype,
             load_in_8bit = args.load_in_8bit,
             max_new_tokens = args.max_new_tokens,
