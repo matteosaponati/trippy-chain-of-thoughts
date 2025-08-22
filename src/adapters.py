12531ab679b6b8881a78e3676463befdf0ce7035
matteosaponati <matteosaponati@gmail.com>
2025-08-22 16:12:05 +0200

    remove unused adapters for MATH dataset (we are not gonna use this dataset anyway)

--- src/adapters.py
+++ src/adapters.py
@@ -1,6 +1,5 @@
 # adapters/gsm8k.py
-from datasets import load_dataset, concatenate_datasets
-import re
+from datasets import load_dataset
 
 def iter_gsm8k(split = "train"):
     ds = load_dataset("gsm8k", "main", split = split)
@@ -17,26 +16,3 @@ def iter_gsm8k(split = "train"):
                    task_type = "math",
                    question = q,
                    gold_answer = gold)
-
-def iter_math(split="train"):
-
-    subjects = ['algebra', 'counting_and_probability', 'geometry', 
-        'intermediate_algebra', 'number_theory', 'prealgebra', 'precalculus']
-    datasets = []
-    for subject in subjects:
-        ds = load_dataset("EleutherAI/hendrycks_math", subject, split = split)
-        datasets.append(ds)
-    combined_ds = concatenate_datasets(datasets)
-
-    for i, ex in enumerate(ds):
-        q = ex["problem"]
-        solution = ex["solution"]
-        boxed_match = re.search(r'\\boxed\{([^}]*)\}', solution)
-        gold = boxed_match.group(1) if boxed_match else None
-        
-        yield dict(
-            uid = f"math/{split}/{i}",
-            task_type = "math",
-            question = q,
-            solution = solution,
-            gold_answer = gold)
