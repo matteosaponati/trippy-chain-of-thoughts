1cd02f62eec32396518f25f5ab2dd05046967d84
matteosaponati <matteosaponati@gmail.com>
2025-08-23 13:06:48 +0200

    add temporary adapters for trippy dataset

--- src/adapters.py
+++ src/adapters.py
@@ -12,7 +12,22 @@ def iter_gsm8k(split = "train"):
         else:
             gold = ex["answer"].strip()
 
-        yield dict(uid = f"gsm8k/{split}/{i}",
+        yield dict(id = f"gsm8k/{split}/{i}",
+                   task_type = "math",
+                   question = q,
+                   gold_answer = gold)
+        
+def iter_trippy(ds):
+    
+    for i, ex in enumerate(ds):
+        q = ex["question"]
+        
+        if "####" in ex["answer"]:
+            gold = ex["answer"].split("####")[-1].strip()
+        else:
+            gold = ex["answer"].strip()
+
+        yield dict(id = f"gsm8k/test/{i}",
                    task_type = "math",
                    question = q,
                    gold_answer = gold)
